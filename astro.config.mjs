51dde9882dd2bdd453823eafd0780db51ec89113
Luis <lramirez41@ucol.mx>
2026-04-21 15:58:02 -0600

    Fix: Set trailingSlash to 'always' to match server configuration and fix sitemap redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://proyectoscsi.mx',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   integrations: [
     tailwind(),
     react(),
